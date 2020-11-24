8ed9af35b9f77ab7731d72ce90e85592bcb1061d
adarista012 <ad.arista012@gmail.com>
2020-11-24 15:06:50 -0400

    custom bar witget

---
+++ lib/UI/widgets/custom_app_bar.dart
@@ -0,0 +1,33 @@
+class CustomAppBar extends PreferredSize {
+  final Widget child;
+  final double height;
+
+  CustomAppBar({@required this.child, this.height = kToolbarHeight});
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: preferredSize.height,
+      //color: Colors.cyan,
+      decoration: BoxDecoration(
+        gradient: new LinearGradient(
+            colors: [
+              workersSecondary,
+              workersColorButton,
+            ],
+            begin: const FractionalOffset(1, -1),
+            end: const FractionalOffset(0.0, 1),
+            stops: [0, 1],
+            tileMode: TileMode.clamp),
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(140),
+            bottomRight: Radius.circular(140)),
+      ),
+      alignment: Alignment.center,
+      child: child,
+    );
+  }
+}
---
+++ lib/global.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+Color lightGreen = Color(0xFF95E08E);
+Color lightBlueIsh = Color(0xFF33BBB5);
+Color darkGreen = Color(0xFF00AA12);
+Color backgroundColor = Color(0xFFEFEEF5);
+Color workersPrimary = Color(0xFFFBB03B);
+Color workersSecondary = Color(0xFFFFFF8E);
+Color workersColorButton = Color(0xFFFFCC4E);
+
+TextStyle textBrownMidle = new TextStyle(
+  fontFamily: 'avenir',
+  color: Colors.brown,
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle titleStyleWhite = new TextStyle(
+    fontFamily: 'Helvetica',
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 25);
+TextStyle jobCardTitileStyleBlue = new TextStyle(
+    fontFamily: 'Avenir',
+    color: lightBlueIsh,
+    fontWeight: FontWeight.bold,
+    fontSize: 12);
+TextStyle jobCardTitileStyleBlack = new TextStyle(
+    fontFamily: 'Avenir',
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 25);
+TextStyle titileStyleLighterBlack = new TextStyle(
+  fontFamily: 'Avenir',
+  color: Color(0xFF34475D),
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+);
+
+TextStyle titileStyleBlack = new TextStyle(
+    fontFamily: 'Helvetica',
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontSize: 20);
+TextStyle salaryStyle = new TextStyle(
+    fontFamily: 'Avenir',
+    color: darkGreen,
+    fontWeight: FontWeight.bold,
+    fontSize: 12);
+TextStyle titleBoxWorkers = new TextStyle(
+  fontFamily: 'Helvetica',
+  color: Colors.black,
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  shadows: [
+    Shadow(
+      color: Colors.black45,
+      offset: Offset(0, 3),
+      blurRadius: 6,
+    ),
+  ],
+);
+TextStyle labelBoxWorkers = new TextStyle(
+  fontFamily: 'Helvetica',
+  color: Colors.brown,
+  //color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
+TextStyle labelBoxWorkers2 = new TextStyle(
+  fontFamily: 'Helvetica',
+  color: Colors.white,
+  //color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  shadows: [
+    Shadow(
+      color: Colors.grey,
+      offset: Offset(0, 2),
+      blurRadius: 2,
+    ),
+  ],
+);
+TextStyle titileCategories = new TextStyle(
+  fontFamily: 'Avenir',
+  color: Colors.brown,
+  //color: Color(0xFF34475D),
+  fontWeight: FontWeight.w900,
+  fontSize: 8,
+);
